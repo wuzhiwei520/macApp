82390fdff50908d110b727dfaf88847ec97a197a
时凯周 <shikaizhou@leavealone.local>
2019-03-19 15:40:14 +0800

    Initial Commit

---
+++ MacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacApp
+//
+//  Created by 时凯周 on 2019/3/19.
+//  Copyright © 2019年 Rongbeiwang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
